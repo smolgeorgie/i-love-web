d083864886c545bbf8bb853963d8f3b12ae329a7
smolgeorgie <ilona.van.oosbree@hva.nl>
2024-10-16 16:41:00 +0200

    footer ingeladen en tekt ready

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,10 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/style.css");
   // Return your Object options:
   return {
     dir: {
       input: "src", // default is "."
-      output: "_site" // default is "_site"
-    }
-  }
+      output: "_site", // default is "_site"
+    },
+  };
 };
-
-//Pass through files
-eleventyConfig.addPassthroughCopy('./src/style.css');
